e5405b1e047f6d4761fbc7c151e4aabb94984472
mwichabe <mwichabecollins@gmail.com>
2026-06-04 20:35:37 +0300

    Fixed cors error

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/weather-proxy': {
+        target: 'https://api.weather-ai.co',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/weather-proxy/, ''),
+        secure: true,
+      },
+    },
+  },
 });
